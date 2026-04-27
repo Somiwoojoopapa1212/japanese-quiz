5728055fabb0eb2c63181a4d8ab2ce1dd820beee
Somiwoojoopapa1212 <shuttle1207@gmail.com>
2026-04-27 15:02:35 +0900

    feat: 퀴즈 중단 버튼 + PWA 설치 지원

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE = 'japanese-quiz-v1';
+const ASSETS = [
+  '/japanese-quiz/',
+  '/japanese-quiz/index.html',
+  '/japanese-quiz/manifest.json',
+  '/japanese-quiz/sentences.json',
+  '/japanese-quiz/icons/icon-192.png',
+  '/japanese-quiz/icons/icon-512.png',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
